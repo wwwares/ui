0bee124829cee0e4a6aab7c9278143f53b9f3821
Evan Kysley <evan@kysley.com>
2025-09-01 21:47:23 -0400

    chore: downgrade tsup and update package configs

---
+++ packages/ui/tsup.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+	entry: ["./src/index.ts"],
+	format: ["esm", "cjs"],
+	// dts: true,
+	external: ["react", "react-dom"],
+	watch: ["src/**/*.(ts,tsx)"],
+});
